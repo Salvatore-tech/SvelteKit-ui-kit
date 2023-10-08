f45da49e52423ff633299f6dffa63fb02ee1b5dc
salvatorestarace <salvatore.tech99@gmail.com>
2023-10-08 11:38:51 +0200

    Demo scaffolding for ui-kit components

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sveltekit ui-kit
+
+Reusable and responsive ui-kit components for your next SvelteKit project. 
+
+## Components
+
+1. **Card**: go to /card to see a demo ![Screenshot](static/demo/card/img.png)
+
+
+## Building
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+```
+
+## Developing
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+To create a production version of the lib:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
---
+++ src/routes/card/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import CardComponent from '../../components/CardComponent.svelte';
+
+
+    const preTitle = 'New';
+    const title = 'New card component!'
+    const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae rem.'
+    const author = 'By Salvatore'
+    const variantFilling = 'variant-filled-surface';
+    const imgLink1 = 'static/cardsImg/daniel-cheung.jpg'
+    const imgLink2 = 'static/cardsImg/musa-ortac.jpg'
+    const imgLink3 = 'static/cardsImg/priscilla-du-preez.jpg'
+    const imgLink4 = 'static/cardsImg/jessica-knowlden.jpg'
+</script>
+
+<div class="cardContainer">
+    <CardComponent {preTitle} {title} {description} {author} {variantFilling} imgLink={imgLink1}/>
+    <CardComponent {preTitle} {title} {description} {author} {variantFilling} imgLink={imgLink2}/>
+    <CardComponent {preTitle} {title} {description} {author} {variantFilling} imgLink={imgLink3}/>
+    <CardComponent {preTitle} {title} {description} {author} {variantFilling} imgLink={imgLink4}/>
+</div>
+
+
+<style>
+    .cardContainer{
+        display: grid; /* start with mobile layout */
+        grid-column-gap: 10px; /* space between columns, change as needed */
+        grid-row-gap: 10px; /* space between rows, change as needed */
+    }
+
+    /* tablet, iPad, lo-res laptops ands desktops */
+    @media (min-width:768px)  {
+        .cardContainer {
+            grid-template-columns: repeat(2, 1fr);
+        }  }
+
+    /* big landscape tablets, laptops, and desktops */
+    @media (min-width:1025px) {
+        .cardContainer {
+            grid-template-columns: repeat(3, 1fr);
+        }
+    }
+</style>
